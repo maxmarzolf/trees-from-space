9f3f0a308bf2113131e5663fc322a8032d7f69a4
maxmarzolf <max@treesfromspace.com>
2020-11-23 14:22:58 -0500

    Followed suggestions

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,8 @@
 from website import create_app
-
+import config as configuration
 
 app = create_app()
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=configuration.Development)

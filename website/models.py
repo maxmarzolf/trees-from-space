521a3084d22d983fde18eeab61ca413f0a659ef9
maxmarzolf <max@treesfromspace.com>
2020-11-24 13:31:24 -0500

    Followed model suggestions

--- website/models.py
+++ website/models.py
@@ -1,10 +1,12 @@
 from app import db
+from datetime import datetime
 
 
 class Order(db.Model):
-    id = db.Column(db.Integer(), nullable=False)
-    datetime = db.Column(db.datetime(), nullable=False)
+    __tablename__ = 'ORDER'
+    id = db.Column(db.Integer(), primary_key=True)
+    datetime = db.Column(db.datetime(), nullable=False, default=datetime.now())
     name = db.Column(db.string(150), nullable=False)
     email = db.Column(db.string(150), nullable=False)
     order = db.Column(db.string(150), nullable=False)
-    total = db.Column(db.Numeric, nullable=False)
+    total = db.Column(db.Numeric, nullable=False, precision(8, 4))

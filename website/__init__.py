363dc8a5503e225e57205ee55539d3e863556f71
maxmarzolf <max@treesfromspace.com>
2020-11-23 21:26:37 -0500

    Followed suggestions

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,17 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+migrate = Migrate()
 
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object('config.Development')
+
+    db.init_app(app)
+    migrate.init_app(app, db)
 
     with app.app_context():
         from website.home.home_routes import home_bp

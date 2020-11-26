e8ba20322e4ee82921c723ea75442b444553196b
maxmarzolf <max@treesfromspace.com>
2020-11-25 23:31:16 -0500

    Merge branch 'main' of /Users/max/PycharmProjects/PDM with conflicts.

---
+++ website/__init__.py
@@ -0,0 +1,35 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+from website import models
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('config.Development')
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    with app.app_context():
+        from website.home.home_routes import home_bp
+        app.register_blueprint(home_bp)
+
+        from website.product.product_routes import product_bp
+        app.register_blueprint(product_bp)
+
+        from website.checkout.checkout_routes import checkout_bp
+        app.register_blueprint(checkout_bp)
+
+        from website.payment.payment_routes import payment_bp
+        app.register_blueprint(payment_bp)
+
+        # only called if the tables have not been created yet
+        # db.create_all()
+
+        return app
---
+++ website/models.py
@@ -0,0 +1,24 @@
+from website import db
+from datetime import datetime
+
+
+class Order(db.Model):
+    __tablename__ = 'ORDER'
+    id = db.Column(db.Integer(), primary_key=True)
+    datetime = db.Column(db.DateTime(), nullable=False, default=datetime.now)
+    name = db.Column(db.String(150), nullable=False)
+    email = db.Column(db.String(150), nullable=False)
+    order = db.Column(db.String(150), nullable=False)
+    total = db.Column(db.Numeric(8, 4), nullable=False)
+
+
+class ShirtInventory(db.Model):
+    __tablename__ = 'SHIRT_INVENTORY'
+    small = db.Column(db.Integer())
+    medium = db.Column(db.Integer())
+    large = db.Column(db.Integer())
+
+
+class DecalInventory(db.Model):
+    __tablename__ = 'DECAL_INVENTORY'
+    quantity = db.Column(db.Integer())

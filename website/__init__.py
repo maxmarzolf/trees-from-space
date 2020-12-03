6bdf2cab81be5aba0ed54b27dc57b33fe64d5bda
maxmarzolf <max@treesfromspace.com>
2020-12-02 22:37:45 -0500

    mission changed to about

--- website/__init__.py
+++ website/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from website.home.home_routes import home_bp
-from website.mission.mission_routes import mission_bp
+from website.mission.mission_routes import about_bp
 from website.checkout.checkout_routes import checkout_bp
 from website.payment.payment_routes import payment_bp
 
@@ -19,7 +19,7 @@ def create_app():
     migrate.init_app(app, db)
     with app.app_context():
         app.register_blueprint(home_bp)
-        app.register_blueprint(mission_bp)
+        app.register_blueprint(about_bp)
         app.register_blueprint(checkout_bp)
         app.register_blueprint(payment_bp)
         db.create_all()

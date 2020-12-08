62a0918fbcae2cd9c958eec4eee6fb910abcdedd
maxmarzolf <max@treesfromspace.com>
2020-12-08 16:29:09 -0500

    Model Update

--- website/payment/payment_routes.py
+++ website/payment/payment_routes.py
@@ -1,7 +1,79 @@
-from flask import render_template
 from website.payment import payment_bp
+from config import stripe_keys
+from flask import jsonify, render_template, request
+import stripe
 
 
-@payment_bp.route('/payment')
-def land_here():
-    return render_template("payment/payment.html")
+@payment_bp.route("/config")
+def get_publishable_key():
+    stripe_config = {"publicKey": stripe_keys["publishable_key"]}
+    return jsonify(stripe_config)
+
+
+@payment_bp.route("/create-checkout-session")
+def create_checkout_session():
+    domain_url = "http://localhost:5000/"
+    stripe.api_key = stripe_keys["secret_key"]
+
+    try:
+        # Create new Checkout Session for the order
+        # Other optional params include:
+        # [billing_address_collection] - to display billing address details on the page
+        # [customer] - if you have an existing Stripe Customer ID
+        # [payment_intent_data] - capture the payment later
+        # [customer_email] - prefill the email input in the form
+        # For full details see https://stripe.com/docs/api/checkout/sessions/create
+
+        # ?session_id={CHECKOUT_SESSION_ID} means the redirect will have the session ID set as a query param
+        checkout_session = stripe.checkout.Session.create(
+            success_url=domain_url + "success?session_id={CHECKOUT_SESSION_ID}",
+            cancel_url=domain_url + "cancelled",
+            payment_method_types=["card"],
+            mode="payment",
+            line_items=[
+                {
+                    "name": "Shirt",
+                    "quantity": 1,
+                    "currency": "usd",
+                    "amount": "3000",
+                }
+            ]
+        )
+        return jsonify({"sessionId": checkout_session["id"]})
+    except Exception as e:
+        return jsonify(error=str(e)), 403
+
+
+@payment_bp.route("/success")
+def success():
+    return render_template("payment/success.html")
+
+
+@payment_bp.route("/cancelled")
+def cancelled():
+    return render_template("payment/cancelled.html")
+
+
+@payment_bp.route("/webhook", methods=["POST"])
+def stripe_webhook():
+    payload = request.get_data(as_text=True)
+    sig_header = request.headers.get("Stripe-Signature")
+
+    try:
+        event = stripe.Webhook.construct_event(
+            payload, sig_header, stripe_keys["endpoint_secret"]
+        )
+
+    except ValueError as e:
+        # Invalid payload
+        return "Invalid payload", 400
+    except stripe.error.SignatureVerificationError as e:
+        # Invalid signature
+        return "Invalid signature", 400
+
+    # Handle the checkout.session.completed event
+    if event["type"] == "checkout.session.completed":
+        print("Payment was successful.")
+        # TODO: run some custom code here
+
+    return "Success", 200

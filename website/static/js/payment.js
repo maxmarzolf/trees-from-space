ba1972a388c21068487355c86cfbe2297da2ea39
maxmarzolf <max@treesfromspace.com>
2020-12-19 18:17:48 -0500

    Stripe Development

--- website/static/js/payment.js
+++ website/static/js/payment.js
@@ -1,196 +1,241 @@
-var stripe = Stripe("pk_test_51Hw9NqKQiaq7vSEcKGxwaPwRpqIhGbDvrmXqfDBLUwwv7drXUMnxJdCvEEQxfN52JcES1hvUEOl03BrvJEPa60f500QNVmY7HT");
-var elements = stripe.elements();
+// The max and min number of photos a customer can purchase
+var MIN_SHIRTS = 1;
+var MAX_SHIRTS = 10;
 
-intent = stripe.PaymentIntent.create(
-  amount=1099,
-  currency='usd',
-  // Verify your integration in this guide by including this parameter
-  metadata={'integration_check': 'accept_a_payment'},
+var basicPhotoButton = document.getElementById('basic-photo-button');
+document
+    .getElementById('quantity-input')
+    .addEventListener('change', function (evt) {
+        // Ensure customers only buy between 1 and 10 photos
+        if (evt.target.value < MIN_SHIRTS) {
+            evt.target.value = MIN_SHIRTS;
+        }
+        if (evt.target.value > MAX_SHIRTS) {
+            evt.target.value = MAX_SHIRTS;
+        }
+    });
+
+/* Method for changing the product quantity when a customer clicks the increment / decrement buttons */
+var updateQuantity = function (evt) {
+    if (evt && evt.type === 'keypress' && evt.keyCode !== 13) {
+        return;
+    }
+
+    var isAdding = evt && evt.target.id === 'add';
+    var inputEl = document.getElementById('quantity-input');
+    var currentQuantity = parseInt(inputEl.value);
+
+    document.getElementById('add').disabled = false;
+    document.getElementById('subtract').disabled = false;
+
+    // Calculate new quantity
+    var quantity = evt
+        ? isAdding
+            ? currentQuantity + 1
+            : currentQuantity - 1
+        : currentQuantity;
+    // Update number input with new value.
+    inputEl.value = quantity;
+    // Calculate the total amount and format it with currency symbol.
+    var amount = config.unitAmount;
+    var numberFormat = new Intl.NumberFormat(i18next.language, {
+        style: 'currency',
+        currency: config.currency,
+        currencyDisplay: 'symbol',
+    });
+    var parts = numberFormat.formatToParts(amount);
+    var zeroDecimalCurrency = true;
+    for (var part of parts) {
+        if (part.type === 'decimal') {
+            zeroDecimalCurrency = false;
+        }
+    }
+    amount = zeroDecimalCurrency ? amount : amount / 100;
+    var total = (quantity * amount).toFixed(2);
+    var formattedTotal = numberFormat.format(total);
+
+    document
+        .getElementById('submit')
+        .setAttribute('i18n-options', `{ "total": "${formattedTotal}" }`);
+    updateContent('button.submit');
+
+    // Disable the button if the customers hits the max or min
+    if (quantity === MIN_SHIRTS) {
+        document.getElementById('subtract').disabled = true;
+    }
+    if (quantity === MAX_SHIRTS) {
+        document.getElementById('add').disabled = true;
+    }
+};
+
+/* Attach method */
+Array.from(document.getElementsByClassName('increment-btn')).forEach(
+    (element) => {
+        element.addEventListener('click', updateQuantity);
+    }
 );
 
-var response = fetch('/secret').then(function(response) {
-  return response.json();
-}).then(function(responseJson) {
-  var clientSecret = responseJson.client_secret;
-  // Call stripe.confirmCardPayment() with the client secret.
-});
+/* Handle any errors returns from Checkout  */
+var handleResult = function (result) {
+    if (result.error) {
+        var displayError = document.getElementById('error-message');
+        displayError.textContent = result.error.message;
+    }
+};
 
-// Set up Stripe.js and Elements to use in checkout form
-var elements = stripe.elements();
-var style = {
-  base: {
-    color: "#32325d",
+// Create a Checkout Session with the selected quantity
+var createCheckoutSession = function () {
+    var inputEl = document.getElementById('quantity-input');
+    var quantity = parseInt(inputEl.value);
+
+    return fetch('/create-checkout-session', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            quantity: quantity,
+            locale: i18next.language.toLowerCase().split('-')[0],
+        }),
+    }).then(function (result) {
+        return result.json();
+    });
+};
+
+Array.from(document.getElementsByClassName('increment-btn')).forEach(
+  (element) => {
+    element.addEventListener('click', updateQuantity);
+  }
+);
+
+/* Handle any errors returns from Checkout  */
+var handleResult = function (result) {
+  if (result.error) {
+    var displayError = document.getElementById('error-message');
+    displayError.textContent = result.error.message;
   }
 };
 
-var card = elements.create("card", { style: style });
-card.mount("#card-element");
+// Create a Checkout Session with the selected quantity
+var createCheckoutSession = function () {
+  var inputEl = document.getElementById('quantity-input');
+  var quantity = parseInt(inputEl.value);
 
-card.on('change', ({error}) => {
-  let displayError = document.getElementById('card-errors');
-  if (error) {
-    displayError.textContent = error.message;
-  } else {
-    displayError.textContent = '';
-  }
-});
-
-var form = document.getElementById('payment-form');
-
-form.addEventListener('submit', function(ev) {
-  ev.preventDefault();
-  stripe.confirmCardPayment(clientSecret, {
-    payment_method: {
-      card: card,
-      billing_details: {
-        name: 'Jenny Rosen'
-      }
-    }
-  }).then(function(result) {
-    if (result.error) {
-      // Show error to your customer (e.g., insufficient funds)
-      console.log(result.error.message);
-    } else {
-      // The payment has been processed!
-      if (result.paymentIntent.status === 'succeeded') {
-        // Show a success message to your customer
-        // There's a risk of the customer closing the window before callback
-        // execution. Set up a webhook or plugin to listen for the
-        // payment_intent.succeeded event that handles any business critical
-        // post-payment actions.
-      }
-    }
+  return fetch('/create-checkout-session', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      quantity: quantity,
+      locale: i18next.language.toLowerCase().split('-')[0],
+    }),
+  }).then(function (result) {
+    return result.json();
   });
-});
+};
 
-
-
-
-
-
-// Disable the button until we have Stripe set up on the page
-document.querySelector("button").disabled = true;
-
-fetch("/create-payment-intent", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json"
-  },
-  body: JSON.stringify(orderData)
-})
-  .then(function(result) {
+/* Get your Stripe publishable key to initialize Stripe.js */
+fetch('/config')
+  .then(function (result) {
     return result.json();
   })
-  .then(function(data) {
-    return setupElements(data);
-  })
-  .then(function({ stripe, card, clientSecret }) {
-    document.querySelector("button").disabled = false;
-
-    // Handle form submission.
-    var form = document.getElementById("payment-form");
-    form.addEventListener("submit", function(event) {
-      event.preventDefault();
-      // Initiate payment when the submit button is clicked
-      pay(stripe, card, clientSecret);
+  .then(function (json) {
+    window.config = json;
+    var stripe = Stripe(config.publicKey);
+    updateQuantity();
+    // Setup event handler to create a Checkout Session on submit
+    document.querySelector('#submit').addEventListener('click', function (evt) {
+      createCheckoutSession().then(function (data) {
+        stripe
+          .redirectToCheckout({
+            sessionId: data.sessionId,
+          })
+          .then(handleResult);
+      });
     });
   });
 
-// Set up Stripe.js and Elements to use in checkout form
-var setupElements = function(data) {
-  stripe = Stripe(data.publishableKey);
-  var elements = stripe.elements();
-  var style = {
-    base: {
-      color: "#32325d",
-      fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
-      fontSmoothing: "antialiased",
-      fontSize: "16px",
-      "::placeholder": {
-        color: "#aab7c4"
-      }
-    },
-    invalid: {
-      color: "#fa755a",
-      iconColor: "#fa755a"
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+var x, i, j, l, ll, selElmnt, a, b, c;
+/*look for any elements with the class "sizes":*/
+x = document.getElementsByClassName("sizes");
+l = x.length;
+for (i = 0; i < l; i++) {
+    selElmnt = x[i].getElementsByTagName("select")[0];
+    ll = selElmnt.length;
+    /*for each element, create a new DIV that will act as the selected item:*/
+    a = document.createElement("DIV");
+    a.setAttribute("class", "select-selected");
+    a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+    x[i].appendChild(a);
+    /*for each element, create a new DIV that will contain the option list:*/
+    b = document.createElement("DIV");
+    b.setAttribute("class", "select-items select-hide");
+    for (j = 1; j < ll; j++) {
+        /*for each option in the original select element,
+        create a new DIV that will act as an option item:*/
+        c = document.createElement("DIV");
+        c.innerHTML = selElmnt.options[j].innerHTML;
+        c.addEventListener("click", function (e) {
+            /*when an item is clicked, update the original select box,
+            and the selected item:*/
+            var y, i, k, s, h, sl, yl;
+            s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+            sl = s.length;
+            h = this.parentNode.previousSibling;
+            for (i = 0; i < sl; i++) {
+                if (s.options[i].innerHTML == this.innerHTML) {
+                    s.selectedIndex = i;
+                    h.innerHTML = this.innerHTML;
+                    y = this.parentNode.getElementsByClassName("same-as-selected");
+                    yl = y.length;
+                    for (k = 0; k < yl; k++) {
+                        y[k].removeAttribute("class");
+                    }
+                    this.setAttribute("class", "same-as-selected");
+                    break;
+                }
+            }
+            h.click();
+        });
+        b.appendChild(c);
     }
-  };
-
-  var card = elements.create("card", { style: style });
-  card.mount("#card-element");
-
-  return {
-    stripe: stripe,
-    card: card,
-    clientSecret: data.clientSecret
-  };
-};
-
-/*
- * Calls stripe.confirmCardPayment which creates a pop-up modal to
- * prompt the user to enter extra authentication details without leaving your page
- */
-var pay = function(stripe, card, clientSecret) {
-  changeLoadingState(true);
-
-  // Initiate the payment.
-  // If authentication is required, confirmCardPayment will automatically display a modal
-  stripe
-    .confirmCardPayment(clientSecret, {
-      payment_method: {
-        card: card
-      }
-    })
-    .then(function(result) {
-      if (result.error) {
-        // Show error to your customer
-        showError(result.error.message);
-      } else {
-        // The payment has been processed!
-        orderComplete(clientSecret);
-      }
+    x[i].appendChild(b);
+    a.addEventListener("click", function (e) {
+        /*when the select box is clicked, close any other select boxes,
+        and open/close the current select box:*/
+        e.stopPropagation();
+        closeAllSelect(this);
+        this.nextSibling.classList.toggle("select-hide");
+        this.classList.toggle("select-arrow-active");
     });
-};
+}
 
-/* ------- Post-payment helpers ------- */
+function closeAllSelect(elmnt) {
+    /*a function that will close all select boxes in the document,
+    except the current select box:*/
+    var x, y, i, xl, yl, arrNo = [];
+    x = document.getElementsByClassName("select-items");
+    y = document.getElementsByClassName("select-selected");
+    xl = x.length;
+    yl = y.length;
+    for (i = 0; i < yl; i++) {
+        if (elmnt == y[i]) {
+            arrNo.push(i)
+        } else {
+            y[i].classList.remove("select-arrow-active");
+        }
+    }
+    for (i = 0; i < xl; i++) {
+        if (arrNo.indexOf(i)) {
+            x[i].classList.add("select-hide");
+        }
+    }
+}
 
-/* Shows a success / error message when the payment is complete */
-var orderComplete = function(clientSecret) {
-  // Just for the purpose of the sample, show the PaymentIntent response object
-  stripe.retrievePaymentIntent(clientSecret).then(function(result) {
-    var paymentIntent = result.paymentIntent;
-    var paymentIntentJson = JSON.stringify(paymentIntent, null, 2);
+/*if the user clicks anywhere outside the select box,
+then close all select boxes:*/
+document.addEventListener("click", closeAllSelect);
 
-    document.querySelector(".sr-payment-form").classList.add("hidden");
-    document.querySelector("pre").textContent = paymentIntentJson;
 
-    document.querySelector(".sr-result").classList.remove("hidden");
-    setTimeout(function() {
-      document.querySelector(".sr-result").classList.add("expand");
-    }, 200);
-
-    changeLoadingState(false);
-  });
-};
-
-var showError = function(errorMsgText) {
-  changeLoadingState(false);
-  var errorMsg = document.querySelector(".sr-field-error");
-  errorMsg.textContent = errorMsgText;
-  setTimeout(function() {
-    errorMsg.textContent = "";
-  }, 4000);
-};
-
-// Show a spinner on payment submission
-var changeLoadingState = function(isLoading) {
-  if (isLoading) {
-    document.querySelector("button").disabled = true;
-    document.querySelector("#spinner").classList.remove("hidden");
-    document.querySelector("#button-text").classList.add("hidden");
-  } else {
-    document.querySelector("button").disabled = false;
-    document.querySelector("#spinner").classList.add("hidden");
-    document.querySelector("#button-text").classList.remove("hidden");
-  }
-};
